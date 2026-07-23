5cf97058f0c57d4421fa75ba964236da74b4582b
Anantha Kandrapu <akandrapu@paypal.com>
2026-07-23 16:15:10 -0700

    Attn fwd pass with shapes

--- Learning/day2/program.py
+++ Learning/day2/program.py
@@ -25,10 +25,9 @@ def test_op(batch_size, num_heads, seq_len, head_dim ,causal, dtype=torch.float1
     P = torch.matmul(Q,K.transpose(2,3)) * softmax_scale
     if causal:
         P[:, :, MASK == 0]  = float("-inf")
+        print("P causal", P)
     P = torch.softmax(P.float(), dim=-1).half()
+    print("Attention vals after softmax", P, "Shape of " , P.shape)
     ref_0 = torch.matmul(P,V) # ref_0 is (QKt)V
-    print("Attention sf(QKt)*V ", ref_0)
-    print("Attention sf(QKt)*V Shape", ref_0.shape)
-
-
-print(test_op(1,4,16,2,False))
+    print("Full attention operation ", ref_0, "shape of full attention", ref_0.shape)
+print(test_op(1,4,16,2,True))
---
+++ Learning/day2/tracking.md
@@ -0,0 +1,7 @@
+### Checkpoint 1 : Forward pass output
+
+![alt text](image.png)
+
+
+### Casual Mask applied the lower half is the only exposed values
+![alt text](image-1.png)
