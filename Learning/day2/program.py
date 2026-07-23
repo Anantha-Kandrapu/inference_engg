3ae140c28635e0722663c03bdd18f9ab20c0d88d
Anantha Kandrapu <akandrapu@paypal.com>
2026-07-23 14:40:40 -0700

    do attention add causaul mask

--- Learning/day2/program.py
+++ Learning/day2/program.py
@@ -2,7 +2,7 @@ import torch
 import triton
 import triton.language as tl
 
-def test_op(batch_size, num_heads, seq_len, head_dim ,casual, dtype=torch.float16):
+def test_op(batch_size, num_heads, seq_len, head_dim ,causal, dtype=torch.float16):
     Q = (torch.empty(
         batch_size, num_heads, seq_len, head_dim, dtype=dtype, device="cuda"
     ).normal_(mean=0.0,std=0.5).requires_grad_()
@@ -16,4 +16,11 @@ def test_op(batch_size, num_heads, seq_len, head_dim ,casual, dtype=torch.float1
     ).normal_(mean=0.0,std=0.5).requires_grad_()
     )
     softmax_scale = 1 / (head_dim ** 0.5)
-    dQ = torch.randn_like(Q)
+    dQ = torch.randn_like(Q) # for backward pass
+
+    MASK = torch.tril(torch.ones(seq_len,seq_len),device="cuda")
+    P = torch.matmul(Q,K.transpose(2,3)) * softmax_scale
+    if causal:
+        P[:, :, MASK == 0]  = float("-inf")
+    P = torch.softmax(P.float(), dim=-1).half()
+
