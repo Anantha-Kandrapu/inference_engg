a85ddce40b0827eff416199c5c97cb722c94b288
Anantha Kandrapu <akandrapu@paypal.com>
2026-07-23 16:14:37 -0700

    Updategs

--- Learning/day2/program.py
+++ Learning/day2/program.py
@@ -16,11 +16,19 @@ def test_op(batch_size, num_heads, seq_len, head_dim ,causal, dtype=torch.float1
     ).normal_(mean=0.0,std=0.5).requires_grad_()
     )
     softmax_scale = 1 / (head_dim ** 0.5)
-    dQ = torch.randn_like(Q) # for backward pass
+    # dQ = torch.randn_like(Q) # for backward pass 
 
-    MASK = torch.tril(torch.ones(seq_len,seq_len),device="cuda")
+    print("Q Shape : " , Q.shape)
+    print("K trans Shape : ", K.transpose(2,3).shape)
+    MASK = torch.tril(torch.ones(seq_len,seq_len))
+    print("MASK : ", MASK , "Mask Shape : ", MASK.shape)
     P = torch.matmul(Q,K.transpose(2,3)) * softmax_scale
     if causal:
         P[:, :, MASK == 0]  = float("-inf")
     P = torch.softmax(P.float(), dim=-1).half()
+    ref_0 = torch.matmul(P,V) # ref_0 is (QKt)V
+    print("Attention sf(QKt)*V ", ref_0)
+    print("Attention sf(QKt)*V Shape", ref_0.shape)
 
+
+print(test_op(1,4,16,2,False))
